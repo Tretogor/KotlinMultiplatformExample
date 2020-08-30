3029d8c6790c6373f495115e7b2eb48a242e636d
wcisang <william.zischang.cisang@gmail.com>
2020-08-30 10:12:53 -0300

    add koin di

---
+++ app/src/main/java/com/example/android/kotlinmultiplatform/CustomApplication.kt
@@ -0,0 +1,19 @@
+package com.example.android.kotlinmultiplatform
+
+import android.app.Application
+import com.jetbrains.handson.mpp.mobile.di.commonModule
+import com.jetbrains.handson.mpp.mobile.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+
+class CustomApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin {
+            androidLogger()
+            androidContext(this@CustomApplication)
+            modules(commonModule)
+        }
+    }
+}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 }
 
 kotlin {
+
+     val ktor_version = "1.2.5"
+     val serialization_version = "0.13.0"
+     val slf4j_version = "1.7.28"
+     val koin_version = "3.0.0-alpha-2"
+     val coroutines_version = "1.3.0-RC"
+
     //select iOS target platform depending on the Xcode environment variables
     val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
         if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
@@ -24,10 +31,52 @@ kotlin {
 
     sourceSets["commonMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+        implementation("org.koin:koin-core:$koin_version")
+
+        // Coroutines
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutines_version")
+
+        // Ktor
+        implementation("io.ktor:ktor-client-core:$ktor_version")
+        implementation("io.ktor:ktor-client-json:$ktor_version")
+        implementation("io.ktor:ktor-client-logging:$ktor_version")
+        implementation("io.ktor:ktor-client-serialization:$ktor_version")
+
+        // Serialize
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serialization_version")
     }
 
     sourceSets["androidMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+
+        // Ktor
+        implementation("io.ktor:ktor-client-android:$ktor_version")
+        implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+        implementation("io.ktor:ktor-client-json-jvm:$ktor_version")
+        implementation("io.ktor:ktor-client-logging-jvm:$ktor_version")
+        implementation("io.ktor:ktor-client-serialization-jvm:$ktor_version")
+        implementation("org.slf4j:slf4j-android:$slf4j_version")
+
+        // Serialize
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serialization_version")
+    }
+
+    sourceSets["iosMain"].dependencies {
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutines_version")
+
+        // Ktor
+        implementation("io.ktor:ktor-client-ios:$ktor_version")
+        implementation("io.ktor:ktor-client-core-native:$ktor_version")
+        implementation("io.ktor:ktor-client-json-native:$ktor_version")
+        implementation("io.ktor:ktor-client-logging-native:$ktor_version")
+        implementation("io.ktor:ktor-client-serialization-native:$ktor_version")
+
+        // Serialize
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serialization_version")
+
     }
 }
 
