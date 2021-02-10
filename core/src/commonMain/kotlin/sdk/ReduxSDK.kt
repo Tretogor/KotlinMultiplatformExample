27dd7fca1da8b31009a9e65c7e61e935e6aa3f66
wcisang <william.zischang.cisang@gmail.com>
2021-02-10 14:50:03 -0300

    try to fix static methods

--- core/src/commonMain/kotlin/sdk/ReduxSDK.kt
+++ core/src/commonMain/kotlin/sdk/ReduxSDK.kt
@@ -2,11 +2,14 @@ package com.jetbrains.handson.mpp.mobile.sdk
 
 import com.jetbrains.handson.mpp.mobile.listeners.FrameworkListener
 import com.jetbrains.handson.mpp.mobile.store.Store
+import kotlin.jvm.JvmStatic
 
 class ReduxSDK {
     companion object {
+        @JvmStatic
         lateinit var store: Store
 
+        @JvmStatic
         fun initialize(frameworkListener: FrameworkListener) {
             store = Store().apply {
                 initStore(frameworkListener)
