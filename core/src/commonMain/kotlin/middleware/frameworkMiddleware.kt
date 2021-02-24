aba12c8042dfe4fe40aa6737e99c9ed0c605705f
wcisang <william.zischang.cisang@gmail.com>
2021-02-24 16:20:05 -0300

    improvement frameworkMiddleware code

--- core/src/commonMain/kotlin/middleware/frameworkMiddleware.kt
+++ core/src/commonMain/kotlin/middleware/frameworkMiddleware.kt
@@ -9,6 +9,7 @@ import com.wcisang.kotlinmultiplatform.state.ui.InvalidInputInformation
 import com.wcisang.kotlinmultiplatform.state.ui.SendViewInformation
 import com.wcisang.kotlinmultiplatform.status.InformationStatus
 import com.wcisang.kotlinmultiplatform.util.buildUrl
+import org.reduxkotlin.Store
 import org.reduxkotlin.middleware
 
 private var information: InformationStatus = InformationStatus.Holding
@@ -18,32 +19,44 @@ fun frameworkMiddleware(
 ) = middleware<AppState> { store, next, action ->
     if (action is FrameworkAction) {
         if (action is OpenUrlActionRow) {
-            store.dispatch(GetViewInformation(action.data.query.from))
-            if (information is InformationStatus.Success) {
-                frameworkListener.onOpenUrl(
-                    action.data.url.buildUrl(
-                        action.data.query.name,
-                        (information as? InformationStatus.Success)?.data as String
-                    )
-                )
-                information = InformationStatus.Holding
-            }
+            handleOpenUrlActionRow(frameworkListener, store, action)
         }
     }
 
     if (action is SendViewInformation) {
-        information = if (action.validation != null) {
-            if (action.validation.validate(action.data)) {
-                InformationStatus.Success(action.data)
-            } else {
-                store.dispatch(InvalidInputInformation(action.validation.getErrorMessage()))
-                InformationStatus.Canceled
-            }
-        } else {
-            InformationStatus.Success(action.data)
-        }
-
+        handleResult(store, action)
     }
+
     val result = next(action)
     result
+}
+
+private fun handleOpenUrlActionRow(
+    frameworkListener: FrameworkListener,
+    store: Store<AppState>,
+    action: OpenUrlActionRow
+) {
+    store.dispatch(GetViewInformation(action.data.query.from))
+    if (information is InformationStatus.Success) {
+        frameworkListener.onOpenUrl(
+            action.data.url.buildUrl(
+                action.data.query.name,
+                (information as? InformationStatus.Success)?.data as String
+            )
+        )
+        information = InformationStatus.Holding
+    }
+}
+
+private fun handleResult(store: Store<AppState>, action: SendViewInformation) {
+    information = if (action.validation != null) {
+        if (action.validation.validate(action.data)) {
+            InformationStatus.Success(action.data)
+        } else {
+            store.dispatch(InvalidInputInformation(action.validation.getErrorMessage()))
+            InformationStatus.Canceled
+        }
+    } else {
+        InformationStatus.Success(action.data)
+    }
 }
