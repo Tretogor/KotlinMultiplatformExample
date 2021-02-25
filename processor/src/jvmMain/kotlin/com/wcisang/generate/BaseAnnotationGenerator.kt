47f78a268be6462249aeb556813bb43d5b12e5e6
wcisang <william.zischang.cisang@gmail.com>
2021-02-25 14:49:35 -0300

    improvement processor code

--- processor/src/jvmMain/kotlin/com/wcisang/generate/BaseAnnotationGenerator.kt
+++ processor/src/jvmMain/kotlin/com/wcisang/generate/BaseAnnotationGenerator.kt
@@ -1,5 +1,6 @@
 package com.wcisang.generate
 
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import kotlinx.serialization.modules.SerializersModuleBuilder
@@ -17,13 +18,13 @@ internal abstract class BaseAnnotationGenerator<T: Annotation>() {
     fun generateFile(rows: Set<Element>, kaptKotlinGeneratedDir: String) {
         val fileBuilder = FileSpec.builder(packageName, fileName)
         val funcBuilder = FunSpec.builder("buildPolymorphic${annotationType.simpleName}").receiver(SerializersModuleBuilder::class)
+        val codeBlock = CodeBlock.builder()
         rows.map {
-            funcBuilder.addCode(
-                """
-                    polymorphic($baseClassName::class, ${it.simpleName}::class, ${it.simpleName}.serializer())
-                """.trimMargin()
+            codeBlock.addStatement(
+                "polymorphic($baseClassName::class, ${it.simpleName}::class, ${it.simpleName}.serializer())"
             )
         }
+        funcBuilder.addCode(codeBlock.build())
         fileBuilder.addFunction(funcBuilder.build())
         fileBuilder.build().writeTo(File(kaptKotlinGeneratedDir))
     }
